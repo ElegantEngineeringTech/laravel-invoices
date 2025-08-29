4e3c45a086979929a4d0fe7bafac2529d8e2b24d
Quentin Gabriele <quentin.gabriele@gmail.com>
2025-08-29 12:01:59 +0200

    wip

--- src/Models/Invoice.php
+++ src/Models/Invoice.php
@@ -487,7 +487,7 @@ class Invoice extends Model implements Attachable
             buyer: Buyer::fromArray($this->buyer_information ?? []),
             seller: Seller::fromArray($this->seller_information ?? []),
             description: $this->description,
-            items: $this->items->map(fn ($item) => $item->toPdfInvoiceItem())->all(),
+            items: $this->items->values()->map(fn ($item) => $item->toPdfInvoiceItem())->all(),
             tax_label: $this->getTaxLabel(),
             discounts: $this->getDiscounts(),
             logo: $this->getLogo(),
