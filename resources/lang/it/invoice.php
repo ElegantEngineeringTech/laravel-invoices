c722c650ed163f3d75820474dd62dc274c62b973
Quentin Gabriele <quentin.gabriele@gmail.com>
2026-03-02 15:46:21 -0500

    add it locale

---
+++ resources/lang/it/invoice.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Invoice Language Lines
+    |--------------------------------------------------------------------------
+    */
+    'invoice' => 'Fattura',
+    'serial_number' => 'Numero fattura',
+    'due_at' => 'Scadenza il',
+    'created_at' => 'Creata il',
+    'paid_at' => 'Pagata il',
+    'description' => 'Descrizione',
+    'total_amount' => 'Totale',
+    'tax' => 'IVA',
+    'tax_label' => 'IVA',
+    'subtotal_amount' => 'Subtotale',
+    'subtotal_discounted_amount' => 'Subtotale scontato',
+    'amount' => 'Importo',
+    'unit_price' => 'Prezzo unitario',
+    'quantity' => 'Qtà',
+    'discount_name' => 'Sconto',
+
+    'from' => 'Da',
+    'to' => 'A',
+    'shipping_to' => 'Spedito a',
+
+    'states' => [
+        'draft' => 'Bozza',
+        'pending' => 'In attesa',
+        'paid' => 'Pagata',
+        'refunded' => 'Rimborsata',
+    ],
+
+    'types' => [
+        'invoice' => 'Fattura',
+        'quote' => 'Preventivo',
+        'credit' => 'Nota di credito',
+        'proforma' => 'Fattura Proforma',
+    ],
+
+    'page' => 'Pagina',
+];
